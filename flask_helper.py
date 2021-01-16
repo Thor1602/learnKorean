8a03d030a79d4b741e04959da3a8efce4c127eea
thor1602 <thorbendhaenenstd@gmail.com>
2021-01-16 10:02:57 +0900

    json file updated

--- flask_helper.py
+++ flask_helper.py
@@ -1,4 +1,4 @@
-
+import json
 def ajax_form_to_dict(ajax_request):
     #This function will turn the raw data received from AJAX in a readable dictionary
     ajax_request = str(ajax_request)
@@ -10,3 +10,33 @@ def ajax_form_to_dict(ajax_request):
         elementList = x.split("=")
         formDict[elementList[0]] = elementList[1]
     return formDict
+
+def add_topic(topic_arg, title_arg, description_arg, rules_arg, examples_arg):
+    topic = {}
+    title = {}
+    description = {}
+    rules = {}
+    examples = {}
+    title["Title"] = title_arg
+    description["Description"] = description_arg
+    rules["Rules"] = rules_arg
+    examples["Examples"] = examples_arg
+    topic[topic_arg] = [title, description, rules, examples]
+    return topic
+
+def add_all_topics():
+    topic1 = add_topic("subject_marker", "-가/-이", "Subject Marker", ["Subject ends with a vowel: +이", "Subject ends with a consonant: +가"],
+              ["의사가 사과를 먹어요. The doctor eats an apple.", "선생님이 배를 먹어요. The teacher eats a pear"])
+    topic2 = add_topic("object_marker", "-가/-이", "Object Marker",
+                       ["Subject ends with a vowel: +이", "Subject ends with a consonant: +가"],
+                       ["의사가 사과를 먹어요. The doctor eats an apple.", "선생님이 배를 먹어요. The teacher eats a pear"])
+    all_topics = {}
+    all_topics["all_topics"] = [topic1, topic2]
+    with open('content.json', 'w') as outfile:
+        json.dump(all_topics, outfile)
+add_all_topics()
+def read_topic():
+    with open('content.json') as outfile:
+        data = json.load(outfile)
+
+read_topic()
--- testdb.py
+++ testdb.py
@@ -1,6 +1,1 @@
-import json
 
-with open('content.json') as json_file:
-    data = json.load(json_file)
-    for p in data['subject_marker']:
-        print(p)
